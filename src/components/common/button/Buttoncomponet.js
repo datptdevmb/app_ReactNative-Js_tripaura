543eb658a9bac9e498d33196fc9036c56bac8ebe
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-09-26 23:50:28 +0700

    sửa lại app.txt

---
+++ src/components/common/button/Buttoncomponet.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Buttoncomponet = () => {
+  return (
+    <View>
+      <Text>B</Text>
+    </View>
+  )
+}
+
+export default Buttoncomponet
+
+const styles = StyleSheet.create({})
