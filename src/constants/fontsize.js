d3ae8405a673a9281a4d1570d6e411044c51a41f
datpt <datptdevmb@gmail.com>
2024-09-26 20:17:33 +0700

    dat commit

---
+++ src/constants/fontsize.js
@@ -0,0 +1,7 @@
+export default FONTSIZE = {
+    xs: '12px',    // Dùng cho mô tả hoặc chú thích nhỏ
+    sm: '14px',    // Dùng cho nội dung chính, button
+    md: '16px',    // Dùng cho các tiêu đề phụ, giá tiền
+    lg: '20px',    // Dùng cho tiêu đề chính
+    xl: '24px',    // Dùng cho tiêu đề lớn hơn hoặc quan trọng
+}
