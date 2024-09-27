480c9e60f459157b63f2d23305ba0b92d051a071
datpt <datptdevmb@gmail.com>
2024-09-27 15:57:12 +0700

    add button

--- src/constants/fontsize.js
+++ src/constants/fontsize.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default fontSize = {
     xs: 12,    // Dùng cho mô tả hoặc chú thích nhỏ
     sm: 14,    // Dùng cho nội dung chính, button
@@ -6,14 +6,3 @@ export default fontSize = {
     lg: 20,    // Dùng cho tiêu đề chính
     xl: 24,    // Dùng cho tiêu đề lớn hơn hoặc quan trọng
 }
-=======
-const FONTSIZE = {
-    xs: 12,    
-    sm: 14,    
-    md: 16,    
-    lg: 20,    
-    xl: 24,   
-};
-
-export default FONTSIZE; 
->>>>>>> 45e3ff1bbd828c5bd1cb3fb9e3fd1b750e55030b
