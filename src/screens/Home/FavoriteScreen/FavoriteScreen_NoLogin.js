d25266725841819b24ace79c238fef3d15319c59
NhatHoang09102004 <hoangndnps33773@fpt.edu.vn>
2024-09-28 15:23:15 +0700

    Luu

---
+++ src/screens/Home/FavoriteScreen/FavoriteScreen_NoLogin.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FavoriteScreen_NoLogin = () => {
+  return (
+    <View>
+      <Text>FavoriteScreen_NoLogin</Text>
+    </View>
+  )
+}
+
+export default FavoriteScreen_NoLogin
+
+const styles = StyleSheet.create({})
