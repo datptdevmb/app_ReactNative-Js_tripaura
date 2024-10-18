25656dad02f020226044507db47a13d0d682d67e
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-10-18 15:57:54 +0700

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,36 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import tourReducer from './slices/tour.slice';
+import categoryReducer from './slices/category.slice';
+import favoriteReducer from './slices/favouriteducers';
+import favoriteAdDeleteReducer from './slices/favouriteAddDeleteducers';
+import favouriteDeleteReducer from './slices/favouriteDeleteDucers';
+import loginReducer from './slices/loginreducers';
+import changeUserReducer from './slices/ChangeUserSlice';
+import registerReducer from './slices/registerreducers';
+import provincesReducer from './slices/cityprovince';
+import districtReducer from './slices/district';
 
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import authenReducer from "./slices/auth.slice";
-import tourReducer from "./slices/tour.slice";
-import categoryReducer from './slices/category.slice'
-
-
-
+// Combine reducers
 const rootReducer = combineReducers({
-    tour: tourReducer,
-    auth:authenReducer,
-    category:categoryReducer
+  tour: tourReducer,
+  category: categoryReducer,
+  favorites: favoriteReducer,
+  favoriteAdDelete: favoriteAdDeleteReducer,
+  favouriteDelete: favouriteDeleteReducer,
+  login: loginReducer,
+  changeUser: changeUserReducer,
+  register: registerReducer,
+  provinces: provincesReducer,
+  district: districtReducer,
 });
 
-
+// Configure the store
 const store = configureStore({
-    reducer: {
-        reducer: rootReducer
-    }
-})
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
 export default store;
