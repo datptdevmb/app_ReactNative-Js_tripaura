0fd4c6fc0ffe371484bd13fb57c4cd8a6c4c6a81
minhnhut2306 <nhutchodiennguyen23062004@gmail.com>
2024-11-07 01:41:43 +0700

    sửa

--- src/screens/main/stacks/detail/Detail.js
+++ src/screens/main/stacks/detail/Detail.js
@@ -1,11 +1,11 @@
 import {
-	StatusBar,
-	StyleSheet,
-	Text,
-	View,
-	Animated,
-	TouchableOpacity,
-	ToastAndroid
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableOpacity,
+  ToastAndroid
 } from 'react-native';
 
 import { useEffect, useRef, useState } from 'react';
@@ -31,367 +31,374 @@ import { KiemTraYeuThich, themXoaYeuThichTour } from '../../../../redux/slices/f
 import Toast from '../../../../components/common/toast/Toast';
 import { ROUTES } from '../../../../constants/routes';
 const reviews = [
-	{
-		id: 1,
-		rating: 5,
-		date: '09/09/2024',
-		name: 'datpham',
-		reviewText: 'Một trải nghiệm tuyệt vời đáng để trải nghiệm',
-		imageUrl: 'https://link-to-avatar-image.com/avatar1.jpg',
-	},
-	{
-		id: 2,
-		rating: 4,
-		date: '15/10/2024',
-		name: 'datpham',
-		reviewText: 'Chuyến đi thú vị và đáng nhớ!',
-		imageUrl: 'https://link-to-avatar-image.com/avatar2.jpg',
-	},
+  {
+    id: 1,
+    rating: 5,
+    date: '09/09/2024',
+    name: 'datpham',
+    reviewText: 'Một trải nghiệm tuyệt vời đáng để trải nghiệm',
+    imageUrl: 'https://link-to-avatar-image.com/avatar1.jpg',
+  },
+  {
+    id: 2,
+    rating: 4,
+    date: '15/10/2024',
+    name: 'datpham',
+    reviewText: 'Chuyến đi thú vị và đáng nhớ!',
+    imageUrl: 'https://link-to-avatar-image.com/avatar2.jpg',
+  },
 
 ];
 
 const Detail = ({ navigation, route }) => {
-	const { _id: tourId } = route.params;
-	const dispatch = useDispatch();
+  const tourId = route.params?._id;
 
-	const {
-		tourById,
-		adultTickets,
-		childTickets,
-		totalPrice,
-		loading,
-		selectedDate,
-	} = useSelector((state) => state.reducer.tour);
+  console.log('tourId: ' + tourId);
 
-	const { isTourFavorited, favoritesStatus, message } = useSelector((state) => state.reducer.favorites)
-	const { user } = useSelector((state) => state.reducer.auth);
-	const [showToast, setShowToast] = useState(false);
+  const dispatch = useDispatch();
+  console.log("route.params:", route.params);
 
+  const {
+    tourById,
+    adultTickets,
+    childTickets,
+    totalPrice,
+    loading,
+    selectedDate,
+  } = useSelector((state) => state.reducer.tour);
 
-	const {
-		imges,
-		tourName,
-		description,
-		location,
-		details,
-	} = tourById;
+  const { isTourFavorited, favoritesStatus, message } = useSelector((state) => state.reducer.favorites)
+  const { user } = useSelector((state) => state.reducer.auth);
+  const [showToast, setShowToast] = useState(false);
 
-	const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
-	const translateY = useRef(new Animated.Value(500)).current;
+  const {
+    imges,
+    tourName,
+    description,
+    location,
+    details,
+  } =  tourById|| {};
 
-	const handleIncreaseAdult = () => dispatch(increaseAdultTicket());
-	const handleIncreaseChild = () => dispatch(increaseChildTicket());
-	const handleDecreaseAdult = () => dispatch(decreaseAdultTicket());
-	const handleDecreaseChild = () => dispatch(decreaseChildTicket());
-	const handleBack = () => navigation.goBack();
+  console.log('id',tourById);
+  
 
 
-	const handleFavorite = () => {
-		dispatch(themXoaYeuThichTour(
-			{
-				userId: user.user._id,
-				tourId
-			}
-		))
+  const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
+  const translateY = useRef(new Animated.Value(500)).current;
 
-		if (favoritesStatus === 'success') {
-			setShowToast(true);
-			setTimeout(() => setShowToast(false), 3000);
-		}
-	}
-
-	const handelNavigateToOrder = () => {
-		if (!selectedDate) {
-			console.log('vui lòng chọn ngày')
-			return
-		}
-		if (adultTickets === 0 && childTickets === 0) {
-			console.log('vui lòng chọn số lượng vé')
-			return
-		}
-		navigation.navigate('Order')
-	}
-
-	const handleNavigateToFavorite = () => {
-		navigation.navigate('yeuthich')
-	}
-
-	useEffect(() => {
-		const loadData = async () => {
-			try {
-				await Promise.all([
-					dispatch(fetchTourById({ tourId })),
-					dispatch(KiemTraYeuThich({ userId: user.user._id, tourId }))
-				]);
-			} catch (err) {
-				console.error('Error fetching data:', err);
-			}
-		};
-		loadData();
-	}, [dispatch]);
+  const handleIncreaseAdult = () => dispatch(increaseAdultTicket());
+  const handleIncreaseChild = () => dispatch(increaseChildTicket());
+  const handleDecreaseAdult = () => dispatch(decreaseAdultTicket());
+  const handleDecreaseChild = () => dispatch(decreaseChildTicket());
+  const handleBack = () => navigation.goBack();
 
 
+  const handleFavorite = () => {
+    dispatch(themXoaYeuThichTour(
+      {
+        userId: user.user._id,
+        tourId
+      }
+    ))
 
-	const toggleBottomSheet = () => {
-		setBottomSheetVisible(!bottomSheetVisible);
-		Animated.timing(translateY, {
-			toValue: bottomSheetVisible ? 500 : 0,
-			duration: 300,
-			useNativeDriver: true,
-		}).start();
-	};
+    if (favoritesStatus === 'success') {
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000);
+    }
+  }
 
-	return (
-		<View style={styles.container}>
-			{showToast && favoritesStatus === 'success'&&(
-				<Toast
-					onPress={handleNavigateToFavorite}
-					message={message}
-				/>
-			)}
-			<TouchableOpacity
-				onPress={handleBack}
-				style={styles.btnBack}>
-				<IcleftArrow />
-			</TouchableOpacity>
+  const handelNavigateToOrder = () => {
+    if (!selectedDate) {
+      console.log('vui lòng chọn ngày')
+      return
+    }
+    if (adultTickets === 0 && childTickets === 0) {
+      console.log('vui lòng chọn số lượng vé')
+      return
+    }
+    navigation.navigate('Order')
+  }
 
-			<TouchableOpacity
-				onPress={handleFavorite}
-				style={styles.btnFavorite}>
-				{
-					!isTourFavorited
-						? <Ic_ouFavorite />
-						: <IcFavorite color={'white'} />
-				}
-			</TouchableOpacity>
+  const handleNavigateToFavorite = () => {
+    navigation.navigate('yeuthich')
+  }
 
-			<AnimatedScrollView
-				TopNavBarComponent={tourName && <TopNav tourName={tourName} />}
-				headerImage={!loading && imges
-					? { uri: imges[0] }
-					: { uri: 'https://bizflyportal.mediacdn.vn/bizflyportal/459/347/2020/06/02/17/37/70515910726734841.jpg' }}
-				imageStyle={{
-					height: 243,
-				}}
-			>
-				<View
-					style={{
-						position: 'absolute',
-						height: 243,
-						top: 0,
-						zIndex: 1,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						backgroundColor: 'rgba(0, 0, 0, 0.3)',
-					}}
-				/>
-
-				<StatusBar translucent backgroundColor="transparent" />
-				{loading && (
-					<View style={styles.animatedHeaderLoading}>
-						<LottieView
-							source={require('../../../../assets/lottile/loading.json')}
-							autoPlay
-							loop
-							style={styles.lottieAnimation}
-						/>
-					</View>
-				)}
-
-				{!loading && (
-					<View>
-						<ImageList dataimage={imges} />
-						<View style={styles.tourInfor}>
-							<Text style={styles.tourname}>{tourName}</Text>
-							<LocationInfo location={location?.departure} />
-							<View style={styles.divider} />
-							<Lable lable="Mô tả chuyến đi" />
-							<Text style={styles.bodytext}>{description}</Text>
-							<ReviewList reviews={reviews} />
-						</View>
-						<View style={{ height: 500 }}></View>
-					</View>
-				)}
-			</AnimatedScrollView>
-
-			{/* Button Bottom */}
-			<View style={styles.btnContainer}>
-				<View style={styles.price}>
-					<Text style={styles.textprice}>Giá chỉ từ</Text>
-					<Text style={styles.total}>700.000 VNĐ</Text>
-				</View>
-				<Button
-					style={styles.btn}
-					label="Mua Ngay"
-					onPress={toggleBottomSheet} />
-			</View>
-
-			{/* Backdrop và Bottom Sheet */}
-			{bottomSheetVisible && (
-				<TouchableOpacity style={styles.backdrop} >
-					<Animated.View
-						style={[
-							styles.bottomSheet,
-							{ transform: [{ translateY }] },
-						]}
-					>
-						<View style={styles.InforContainer}>
-							<TouchableOpacity>
-								<Ic_x onPress={toggleBottomSheet} />
-							</TouchableOpacity>
-							<Text style={styles.tourname}>{tourName}</Text>
-							<DepartureDateSelector
-								onSelectDate={(date) => dispatch(setSelectedDate(date))}
-								selectedDate={selectedDate}
-								data={details} />
-
-							<TicketSelector
-								onIncreaseAdult={handleIncreaseAdult}
-								onIncreaseChild={handleIncreaseChild}
-								onDecreaseAdult={handleDecreaseAdult}
-								onDecreaseChild={handleDecreaseChild}
-								adultPrice={details[0].priceAdult}
-								childPrice={details[0].priceChildren}
-								childTickets={childTickets}
-								adultTickets={adultTickets}
-							/>
-							<RefundPolicy />
-						</View>
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        await Promise.all([
+          dispatch(fetchTourById({ tourId })),
+          dispatch(KiemTraYeuThich({ userId: user.user._id, tourId }))
+        ]);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+      }
+    };
+    loadData();
+  }, [dispatch]);
 
 
-						<View style={styles.btnContainer}>
-							<View style={styles.price}>
-								<Text style={styles.textprice}>Giá chỉ từ</Text>
-								<Text style={styles.total}>{formatCurrencyVND(totalPrice)}</Text>
-							</View>
-							<Button
-								style={styles.btn} label="Mua Ngay"
-								onPress={handelNavigateToOrder} />
-						</View>
-					</Animated.View>
-				</TouchableOpacity>
-			)}
-		</View>
-	);
+
+  const toggleBottomSheet = () => {
+    setBottomSheetVisible(!bottomSheetVisible);
+    Animated.timing(translateY, {
+      toValue: bottomSheetVisible ? 500 : 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <View style={styles.container}>
+      {showToast && favoritesStatus === 'success' && (
+        <Toast
+          onPress={handleNavigateToFavorite}
+          message={message}
+        />
+      )}
+      <TouchableOpacity
+        onPress={handleBack}
+        style={styles.btnBack}>
+        <IcleftArrow />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={handleFavorite}
+        style={styles.btnFavorite}>
+        {
+          !isTourFavorited
+            ? <Ic_ouFavorite />
+            : <IcFavorite color={'white'} />
+        }
+      </TouchableOpacity>
+
+      <AnimatedScrollView
+        TopNavBarComponent={tourName && <TopNav tourName={tourName} />}
+        headerImage={!loading && imges
+          ? { uri: imges[0] }
+          : { uri: 'https://bizflyportal.mediacdn.vn/bizflyportal/459/347/2020/06/02/17/37/70515910726734841.jpg' }}
+        imageStyle={{
+          height: 243,
+        }}
+      >
+        <View
+          style={{
+            position: 'absolute',
+            height: 243,
+            top: 0,
+            zIndex: 1,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.3)',
+          }}
+        />
+
+        <StatusBar translucent backgroundColor="transparent" />
+        {loading && (
+          <View style={styles.animatedHeaderLoading}>
+            <LottieView
+              source={require('../../../../assets/lottile/loading.json')}
+              autoPlay
+              loop
+              style={styles.lottieAnimation}
+            />
+          </View>
+        )}
+
+        {!loading && (
+          <View>
+            <ImageList dataimage={imges} />
+            <View style={styles.tourInfor}>
+              <Text style={styles.tourname}>{tourName}</Text>
+              <LocationInfo location={location?.departure} />
+              <View style={styles.divider} />
+              <Lable lable="Mô tả chuyến đi" />
+              <Text style={styles.bodytext}>{description}</Text>
+              <ReviewList reviews={reviews} />
+            </View>
+            <View style={{ height: 500 }}></View>
+          </View>
+        )}
+      </AnimatedScrollView>
+
+      {/* Button Bottom */}
+      <View style={styles.btnContainer}>
+        <View style={styles.price}>
+          <Text style={styles.textprice}>Giá chỉ từ</Text>
+          <Text style={styles.total}>700.000 VNĐ</Text>
+        </View>
+        <Button
+          style={styles.btn}
+          label="Mua Ngay"
+          onPress={toggleBottomSheet} />
+      </View>
+
+      {/* Backdrop và Bottom Sheet */}
+      {bottomSheetVisible && (
+        <TouchableOpacity style={styles.backdrop} >
+          <Animated.View
+            style={[
+              styles.bottomSheet,
+              { transform: [{ translateY }] },
+            ]}
+          >
+            <View style={styles.InforContainer}>
+              <TouchableOpacity>
+                <Ic_x onPress={toggleBottomSheet} />
+              </TouchableOpacity>
+              <Text style={styles.tourname}>{tourName}</Text>
+              <DepartureDateSelector
+                onSelectDate={(date) => dispatch(setSelectedDate(date))}
+                selectedDate={selectedDate}
+                data={details} />
+
+              <TicketSelector
+                onIncreaseAdult={handleIncreaseAdult}
+                onIncreaseChild={handleIncreaseChild}
+                onDecreaseAdult={handleDecreaseAdult}
+                onDecreaseChild={handleDecreaseChild}
+                adultPrice={details[0].priceAdult}
+                childPrice={details[0].priceChildren}
+                childTickets={childTickets}
+                adultTickets={adultTickets}
+              />
+              <RefundPolicy />
+            </View>
+
+
+            <View style={styles.btnContainer}>
+              <View style={styles.price}>
+                <Text style={styles.textprice}>Giá chỉ từ</Text>
+                <Text style={styles.total}>{formatCurrencyVND(totalPrice)}</Text>
+              </View>
+              <Button
+                style={styles.btn} label="Mua Ngay"
+                onPress={handelNavigateToOrder} />
+            </View>
+          </Animated.View>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
 };
 
 export default Detail;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'white',
-	},
-	animatedHeaderLoading: {
-		backgroundColor: 'black',
-		position: 'absolute',
-		width: '100%',
-		height: 243,
-		zIndex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	InforContainer: {
-		padding: 16
-	},
-	tourInfor: {
-		paddingHorizontal: 16,
-	},
-	btnFavorite: {
-		position: "absolute",
-		top: 40,
-		zIndex: 2,
-		right: 24
-	},
-	btnBack: {
-		position: "absolute",
-		top: 40,
-		zIndex: 2,
-		left: 24
-	},
-	mrtop_12: {
-		marginTop: 12
-	},
-	tourname: {
-		marginTop: 14,
-		color: "#212121",
-		fontSize: 18,
-		lineHeight: 27,
-		fontFamily: 'Poppins-Bold',
-		fontStyle: 'normal',
-		fontWeight: '700',
-	},
-	bodytext: {
-		marginTop: 14,
-		fontSize: 12,
-		fontFamily: 'Poppins_Regular',
-		lineHeight: 18,
-		letterSpacing: 0.4,
-		fontWeight: '400',
-		color: '#494B4B',
-	},
-	lottieAnimation: {
-		width: 100,
-		zIndex: 3,
-		height: 100,
-	},
-	btnContainer: {
-		height: 100,
-		justifyContent: 'center',
-		width: '100%',
-		position: 'absolute',
-		bottom: 0,
-		backgroundColor: 'white',
-		shadowColor: 'black',
-		shadowOffset: { width: 0, height: -1 },
-		shadowOpacity: 0.25,
-		shadowRadius: 1,
-		elevation: 30,
-	},
-	bottomSheet: {
-		position: 'absolute',
-		bottom: 0,
-		width: '100%',
-		height: 600,
-		backgroundColor: 'white',
-		borderTopLeftRadius: 20,
-		borderTopRightRadius: 20,
-		shadowColor: '#000',
-		shadowOffset: { width: 0, height: -2 },
-		shadowOpacity: 0.3,
-		shadowRadius: 10,
-		elevation: 5,
-	},
-	price: {
-		position: 'absolute',
-		left: 16,
-	},
-	textprice: {
-		fontSize: 12,
-		color: 'black',
-		fontWeight: 'bold',
-	},
-	total: {
-		fontSize: 16,
-		color: '#2196F3',
-	},
-	btn: {
-		width: 148,
-		height: 44,
-		backgroundColor: '#2196F3',
-		position: 'absolute',
-		right: 16,
-	},
-	divider: {
-		backgroundColor: "#ededed",
-		width: "100%",
-		height: 2,
-		marginVertical: 10,
-	},
-	backdrop: {
-		// ...StyleSheet.absoluteFillObject,
-		// backgroundColor: 'rgba(0, 0, 0, 0.5)',
-		// justifyContent: 'flex-end',
-	}
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  animatedHeaderLoading: {
+    backgroundColor: 'black',
+    position: 'absolute',
+    width: '100%',
+    height: 243,
+    zIndex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  InforContainer: {
+    padding: 16
+  },
+  tourInfor: {
+    paddingHorizontal: 16,
+  },
+  btnFavorite: {
+    position: "absolute",
+    top: 40,
+    zIndex: 2,
+    right: 24
+  },
+  btnBack: {
+    position: "absolute",
+    top: 40,
+    zIndex: 2,
+    left: 24
+  },
+  mrtop_12: {
+    marginTop: 12
+  },
+  tourname: {
+    marginTop: 14,
+    color: "#212121",
+    fontSize: 18,
+    lineHeight: 27,
+    fontFamily: 'Poppins-Bold',
+    fontStyle: 'normal',
+    fontWeight: '700',
+  },
+  bodytext: {
+    marginTop: 14,
+    fontSize: 12,
+    fontFamily: 'Poppins_Regular',
+    lineHeight: 18,
+    letterSpacing: 0.4,
+    fontWeight: '400',
+    color: '#494B4B',
+  },
+  lottieAnimation: {
+    width: 100,
+    zIndex: 3,
+    height: 100,
+  },
+  btnContainer: {
+    height: 100,
+    justifyContent: 'center',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: -1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 1,
+    elevation: 30,
+  },
+  bottomSheet: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 600,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  price: {
+    position: 'absolute',
+    left: 16,
+  },
+  textprice: {
+    fontSize: 12,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  total: {
+    fontSize: 16,
+    color: '#2196F3',
+  },
+  btn: {
+    width: 148,
+    height: 44,
+    backgroundColor: '#2196F3',
+    position: 'absolute',
+    right: 16,
+  },
+  divider: {
+    backgroundColor: "#ededed",
+    width: "100%",
+    height: 2,
+    marginVertical: 10,
+  },
+  backdrop: {
+    // ...StyleSheet.absoluteFillObject,
+    // backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    // justifyContent: 'flex-end',
+  }
 });
